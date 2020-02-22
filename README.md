13bfb0a9174bfde0e15f3e233653ef1b6ea0f3d2
Aliaksei <45916819+AliakseiVaryvada@users.noreply.github.com>
2020-02-22 20:21:52 +0300

    update: add mock log

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# avaryvada-department-management
+![Java CI](https://github.com/Brest-Java-Course-2020/afedasiuk-department-mngmnt/workflows/Java%20CI/badge.svg)
+
+# afedasiuk-department-mngmnt  
+Department Management demo application
+
+
+## How build
+Setup java 8 and Maven, see [enviroment_setup.md](enviroment_setup.md) 
+  
+    
+## Build project 
+Goto Project folder and execute  
+    
+    mvn clean install
---
+++ department-management-dao/src/test/java/com/epam/brest/courses/dao/DepartmentJdbcDaoImplTest.java
@@ -0,0 +1,65 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.Department;
+import org.graalvm.compiler.core.common.cfg.CFGVerifier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
+public class DepartmentJdbcDaoImplTest {
+
+    @InjectMocks//создаем кземпля класса
+    private DepartmentJdbcDaoImpl departmentDao;
+
+    @Mock
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @AfterEach//вызывается после каждого теста, проверка
+    void after(){
+        Mockito.verifyNoMoreInteractions(namedParameterJdbcTemplate);//проверка вызывается етот метод или нет
+
+    }
+
+    @Test
+    public void getDepartments() {
+//запрос не сраатывает нужен мок
+        List<Department> departments = departmentDao.getDepartments();
+        assertNotNull(departments);
+
+        Mockito.verify(namedParameterJdbcTemplate).query(ArgumentMatchers.anyString(),// в строку грузить можно что угодно через иклс eq
+                ArgumentMatchers.any(RowMapper.class));//за скобкой важно
+    }
+
+    @Test
+    public void getDepartmentById() {
+    }
+
+    @Test
+    public void addDepartment() {
+    }
+
+    @Test
+    public void updateDepartment() {
+    }
+
+    @Test
+    public void deleteDepartment() {
+    }
+
+}
